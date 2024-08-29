4591077276abe975b03df88b297ad899ac7440e7
Lancea <lanceatapu@gmail.com>
2024-08-29 16:07:57 -0700

    Implemented geoDimentions function

--- auth/helpers/waterMarker.js
+++ auth/helpers/waterMarker.js
@@ -3,12 +3,22 @@ module.exports = async (req,res,next) => {
     if (!req.files.length) {
         return next()
     }
-
     const options = {
         ratio: 0.6,
         opacity: 0.4,
         text: 'K I N D E E M',
         textSize: Jimp.FONT_SANS_64_BLACK,
+    }
+    const getDimensions = (H, W, h, w, ratio) => {
+        let hh, ww;
+        if ((H / W) < (h / w)) {    //GREATER HEIGHT
+            hh = ratio * H;
+            ww = hh / h * w;
+        } else {                //GREATER WIDTH
+            ww = ratio * W;
+            hh = ww / w * h;
+        }
+        return [hh, ww];
     }
 
     let results = req.files.map(async file=>{
@@ -17,7 +27,6 @@ module.exports = async (req,res,next) => {
        
         return main.quality(100).write(imagePath);
     })
-    
     await Promise.all(results)
     next()
 }
