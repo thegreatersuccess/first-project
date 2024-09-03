d7d69f9c0947d3659284b115673ec28e89fa294e
Lancea <lanceatapu@gmail.com>
2024-09-02 17:47:25 -0700

    Fixed issue for geoDistance: missing sin operator

--- auth/helpers/geoDistance.js
+++ auth/helpers/geoDistance.js
@@ -3,12 +3,14 @@ module.exports = (lon1, lat1, lon2, lat2) => {
   const R = 6371;
   // distance between latitude and longitude in radians
   const dLat = ((lat2 - lat1) * Math.PI) / 180;
+  const dLon = ((lon2 - lon1) * Math.PI) / 180;
   // haversine’ formula to calculate distance
   const a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
     Math.cos((lat1 * Math.PI) / 180) *
-      Math.cos((lat2 * Math.PI) / 180);
-
+      Math.cos((lat2 * Math.PI) / 180) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   const d = R * c;
   // if (d > 1) return Math.round(d) + "km";
