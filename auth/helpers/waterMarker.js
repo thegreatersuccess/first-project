31e47d819bad38573a3447c227b2f02d02f3db12
Lancea <lanceatapu@gmail.com>
2024-09-09 11:22:08 -0700

    Fixed issue for watermarker functionality

--- auth/helpers/waterMarker.js
+++ auth/helpers/waterMarker.js
@@ -1,14 +1,17 @@
 const Jimp = require('jimp');
+
 module.exports = async (req,res,next) => {
     if (!req.files.length) {
         return next()
     }
+
     const options = {
         ratio: 0.6,
         opacity: 0.4,
         text: 'K I N D E E M',
         textSize: Jimp.FONT_SANS_64_BLACK,
     }
+
     const getDimensions = (H, W, h, w, ratio) => {
         let hh, ww;
         if ((H / W) < (h / w)) {    //GREATER HEIGHT
@@ -18,6 +21,7 @@ module.exports = async (req,res,next) => {
             ww = ratio * W;
             hh = ww / w * h;
         }
+
         return [hh, ww];
     }
     let results = req.files.map(async file=>{
@@ -26,16 +30,19 @@ module.exports = async (req,res,next) => {
 
         const main = await Jimp.read(imagePath);
         const [newHeight, newWidth] = getDimensions(main.getHeight(), main.getWidth(), watermark.getHeight(), watermark.getWidth(), options.ratio);
-        watermark.resize(newWidth, newHeight);
         const positionX = ((main.getWidth() - newWidth) / 2)+250;    
         const positionY = ((main.getHeight() - newHeight) / 2+200);  
+        
+        watermark.resize(newWidth, newHeight);
         watermark.opacity(options.opacity);
+
         main.composite(watermark,
             positionX,
             positionY,
             Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE);
         return main.quality(100).write(imagePath);
     })
+    
     await Promise.all(results)
     next()
 }
